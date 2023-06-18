d4800438238b6c6b0e2221ec3f9097d33f07078f
architchaurasia <droidgraphix7@gmail.com>
2023-06-19 00:38:28 +0530

    update

--- 06. Exploring the world/src/Component/Body.js
+++ 06. Exploring the world/src/Component/Body.js
@@ -1,35 +1,52 @@
 //Here were using props
 import RestaurantCard from "./RestaurantCard";
-import { restaurantList } from "../../Constant";
+import Shimmer from "./Shimmer";
+// import { restaurantList } from "../../Constant";
 import { useEffect, useState } from "react";
 
 function filterData(searchText, restaurants) {
-  const filterData = restaurants.filter((restaurant) =>
-    restaurant.data.name.includes(searchText)
+  const filterData = restaurants?.filter((restaurant) =>
+    restaurant?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase())
   );
   return filterData;
 }
 
 const Body = () => {
-  const [restaurants, setRestaurants] = useState(restaurantList);
+  const [allRestaurants, setAllRestaurants] = useState([]);
+  const [filterdRestaurants, setFilteredRestaurants] = useState([]);
 
   const [searchText, setSearchText] = useState(""); //returns =[variable name, function to update]
   //? Empty dependency array => once after render
   //? dependency array[searchText] => once after initial render + everytime after render (my searchText changes)
+  console.log("render");
   useEffect(() => {
     //API
-    getRestaurants()
+    getRestaurants();
   }, []);
 
   async function getRestaurants() {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6063669&lng=77.3252633&page_type=DESKTOP_WEB_LISTING"
     );
+    console.log(data);
     const json = await data.json();
     console.log(json);
+    //Optional chaining
+    setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+    setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
   }
+  //? Condidtional rendering
+  //If rest. no data - Shimmer UI
+  //Else show data
 
-  return (
+  if (!allRestaurants) return null;
+
+  // if (filterdRestaurants?.length === 0) return <h1>Not Restaurant Found!!</h1>;
+
+  //Not return component(Early return)
+  return allRestaurants?.length === 0 ? (
+    <Shimmer />
+  ) : (
     <>
       <div className="searchContainer">
         <input
@@ -45,9 +62,9 @@ const Body = () => {
           className="search-btn"
           onClick={() => {
             //need to filter the data
-            const data = filterData(searchText, restaurants);
+            const data = filterData(searchText, allRestaurants);
             //update thestate restaurants
-            setRestaurants(data);
+            setFilteredRestaurants(data);
           }}
         >
           Search
@@ -55,10 +72,10 @@ const Body = () => {
       </div>
 
       <div className="cardlist">
-        {restaurants.map((restaurant) => {
+        {filterdRestaurants.map((restaurant) => {
           return (
             <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
-          );
+          )
         })}
       </div>
     </>
---
+++ 06. Exploring the world/src/Component/Shimmer.js
@@ -0,0 +1,9 @@
+// import { ,Breathing } from "react-shimmer";
+
+const Shimmer = () => {
+  return (
+    <h1>Loading.....</h1>
+  );
+};
+
+export default Shimmer;

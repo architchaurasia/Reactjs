4432e53df08e0ea172d3f3b6e9485d6f992e87e6
architchaurasia <droidgraphix7@gmail.com>
2023-08-02 22:10:53 +0530

    update

--- 08. Lets get Classy/src/Component/About.js
+++ 08. Lets get Classy/src/Component/About.js
@@ -1,17 +1,41 @@
-import { Outlet } from "react-router-dom";
+// import { Outlet } from "react-router-dom";
 import Profile from "./ProfileClass";
-import ProfileComponent from "./Profile";
+// import ProfileComponent from "./Profile";
+import React from "react";
 
-const About = () => {
-  return (
+// const About = () => {
+//   return (
+//     <div>
+//       {" "}
+//       <h1>This is About page of Food</h1>
+//       {/* <Outlet /> */}
+//       <Profile name={"Archit Chaurasia"} />
+//       <ProfileComponent name={"Archit"} />
+//     </div>
+//   );
+// };
+
+//*Class base component
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("Parent construtor");
+  }
+  componentDidMount() {
+    //Api Call
+    console.log("Parent componentdidmount");
+  }
+  render() {
+    console.log("Parent render")
+    return(
     <div>
-      {" "}
-      <h1>This is About page of Food</h1>
-      {/* <Outlet /> */}
-      <Profile name={"Archit Chaurasia"} />
-      <ProfileComponent name={"Archit"} />
+      <h1>This is a about page of food</h1>
+      <Profile name={"Archit Chaurasia"} /> {/*It goes into it which we call child*/}
+      {/* <ProfileComponent name={"Archit"} /> */}
     </div>
-  );
-};
+    )
+  }
+}
 
 export default About;
--- 08. Lets get Classy/src/Component/ProfileClass.js
+++ 08. Lets get Classy/src/Component/ProfileClass.js
@@ -8,15 +8,23 @@ class Profile extends React.Component {
       count: 0,
       count2: 3,
     };
+    console.log("child constructor");
   }
+  componentDidMount(){
+    //API call
+    console.log("Child Component did mount");
+}
   render() {
     const { count } = this.state;
+    console.log("child render")
     return (
       <div>
         <h1>Name: {this.props.name}</h1>
         <h2>Count: {count}</h2>
         <button
           onClick={() => {
+            //We do not mutate state directly
+            //Never do this.state = something
             this.setState({ count: 1 });
           }}
         >
